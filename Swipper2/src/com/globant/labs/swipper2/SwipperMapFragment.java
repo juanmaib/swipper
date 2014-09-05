98a505edfbf893468dfdddf3589b8e5b013f5e7b
pauloeojeda <pauloeojeda@gmail.com>
2014-09-05 11:19:11 -0300

    Initial Commit

---
+++ Swipper2/src/com/globant/labs/swipper2/SwipperMapFragment.java
@@ -0,0 +1,46 @@
+package com.globant.labs.swipper2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SwipperMapFragment extends Fragment{
+	
+	private SupportMapFragment fragment;
+	private GoogleMap map;
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		FragmentManager fm = getChildFragmentManager();
+		fragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+		if (fragment == null) {
+			fragment = SupportMapFragment.newInstance();
+			fm.beginTransaction().replace(R.id.map, fragment).commit();
+		}
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (map == null) {
+			map = fragment.getMap();
+			map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+		}
+	}
+
+}
