da0e5b9a8775e1fbfed0a7a70fe92e1d622b0912
Paulo E. Ojeda <paulo.ojeda@globant.com>
2014-11-05 17:06:34 -0300

    Added empty listview message Added a view to be displayed when there are no places to be displayed according to current location and filters.

--- Swipper2/src/com/globant/labs/swipper2/fragments/PlacesListFragment.java
+++ Swipper2/src/com/globant/labs/swipper2/fragments/PlacesListFragment.java
@@ -1,41 +1,103 @@
 package com.globant.labs.swipper2.fragments;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.globant.labs.swipper2.PlaceDetailActivity;
 import com.globant.labs.swipper2.R;
 import com.globant.labs.swipper2.models.Place;
+import com.globant.labs.swipper2.widget.SwipperTextView;
 
 public class PlacesListFragment extends ListFragment implements OnItemClickListener {
 
 	public PlacesListFragment() {
 		super();
 	}
-	
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);   
-    }
-	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	    inflater.inflate(R.menu.list, menu);
-	    super.onCreateOptionsMenu(menu,inflater);
+		inflater.inflate(R.menu.list, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
-		getListView().setDivider(getActivity().getResources().getDrawable(R.drawable.divider));	
+		getListView().setDivider(getActivity().getResources().getDrawable(R.drawable.divider));
 		getListView().setOnItemClickListener(this);
 		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		setUpEmptyListView();
+	}
+
+	private void setUpEmptyListView() {
+		// Can't use a compound drawable here because we can't access
+		// layout_gravity of TextView. Let's do this the old fashion
+
+		// Set up a linear layout container
+		LinearLayout emptyListLayout = new LinearLayout(getActivity());
+		emptyListLayout.setOrientation(LinearLayout.VERTICAL);
+		emptyListLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT));
+		emptyListLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+		emptyListLayout.setPadding(0, dpToPixels(60), 0, 0);
+		emptyListLayout.setBackgroundColor(getResources().getColor(android.R.color.white));
+		emptyListLayout.setVisibility(View.GONE);
+
+		// Set up the image used
+		ImageView imageEmptyList = new ImageView(getActivity());
+		imageEmptyList.setImageResource(R.drawable.info_icon);
+		imageEmptyList.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+
+		// Then the text displayed
+		SwipperTextView textEmptyList = new SwipperTextView(getActivity());
+		textEmptyList.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+		textEmptyList.setGravity(Gravity.CENTER_HORIZONTAL);
+		textEmptyList.setTextColor(getResources().getColor(R.color.button_separator));
+		textEmptyList.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+		textEmptyList.setTypeface(Typeface.createFromAsset(getActivity().getAssets(),
+				"fonts/roboto_light_italic.ttf"));
+		textEmptyList.setPadding(0, dpToPixels(20), 0, 0);
+		textEmptyList.setText(R.string.status_no_places_dual_line);
+
+		// Add both views to the linear layout
+		emptyListLayout.addView(imageEmptyList);
+		emptyListLayout.addView(textEmptyList);
+
+		// And link everything as the view to be displayed when the list is
+		// empty
+		((ViewGroup) getListView().getParent()).addView(emptyListLayout);
+		getListView().setEmptyView(emptyListLayout);
+	}
+
+	public int dpToPixels(int dp) {
+		float scale = getResources().getDisplayMetrics().density;
+		int dpAsPixels = (int) (dp * scale + 0.5f);
+		return dpAsPixels;
 	}
 
 	@Override
@@ -45,8 +107,9 @@ public class PlacesListFragment extends ListFragment implements OnItemClickListe
 		intent.putExtra(PlaceDetailActivity.PLACE_ID_EXTRA, p.getId());
 		intent.putExtra(PlaceDetailActivity.PLACE_NAME_EXTRA, p.getName());
 		intent.putExtra(PlaceDetailActivity.PLACE_CATEGORY_EXTRA, p.getCategory());
-		intent.putExtra(PlaceDetailActivity.PLACE_DISTANCE_EXTRA, ((PlacesAdapter) getListAdapter()).getProvider().getDistanceTo(p));
+		intent.putExtra(PlaceDetailActivity.PLACE_DISTANCE_EXTRA,
+				((PlacesAdapter) getListAdapter()).getProvider().getDistanceTo(p));
 		startActivity(intent);
 	}
-	
+
 }
